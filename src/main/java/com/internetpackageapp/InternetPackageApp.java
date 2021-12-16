f5a66fd033b194dce7d28dac4d39a4916fbea827
Raihan Romzi <>
2021-12-16 17:03:25 +0700

    Finish AddQuota FXML

--- src/main/java/com/internetpackageapp/InternetPackageApp.java
+++ src/main/java/com/internetpackageapp/InternetPackageApp.java
@@ -6,12 +6,11 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.util.Objects;
 
 public class InternetPackageApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(InternetPackageApp.class.getResource("Register.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(InternetPackageApp.class.getResource("AddQuota.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 700, 500);
         stage.setTitle("Tugas Besar PBO IF-43-08");
         stage.setResizable(false);
