0f6afdd47b98c8263bcdf23b2cb271b17355a5fa
Raihan Romzi <>
2021-12-17 04:14:00 +0700

    Step 1 : FXML Done

--- src/main/java/com/internetpackageapp/App.java
+++ src/main/java/com/internetpackageapp/App.java
@@ -7,10 +7,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class InternetPackageApp extends Application {
+public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(InternetPackageApp.class.getResource("ThankYou.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("Home.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 700, 500);
         stage.setTitle("Tugas Besar PBO IF-43-08");
         stage.setResizable(false);
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.internetpackageapp {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
 
 
     opens com.internetpackageapp to javafx.fxml;
