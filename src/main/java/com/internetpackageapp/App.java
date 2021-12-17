580304eb661b8974a0a40d08797f2d8f435de851
Raihan Romzi <>
2021-12-17 09:15:53 +0700

    Add JFoenix Component

--- src/main/java/com/internetpackageapp/App.java
+++ src/main/java/com/internetpackageapp/App.java
@@ -5,12 +5,13 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+
 import java.io.IOException;
 
 public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("Home.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("AddQuota.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 700, 500);
         stage.setTitle("Tugas Besar PBO IF-43-08");
         stage.setResizable(false);
