fb7251a377029796fa9c6dee86f68e77a2112a15
Raihan Romzi <>
2021-12-20 09:29:56 +0700

    Finish Static App

--- src/main/java/com/internetpackageapp/App.java
+++ src/main/java/com/internetpackageapp/App.java
@@ -2,21 +2,25 @@ package com.internetpackageapp;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-
-import java.io.IOException;
+import java.util.Objects;
 
 public class App extends Application {
+
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("Login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 700, 500);
-        stage.setTitle("Tugas Besar PBO IF-43-08");
-        stage.setResizable(false);
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage stage) {
+        try {
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Login.fxml")));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.setTitle("Tugas Besar PBO IF-43-08");
+            stage.setResizable(false);
+            stage.show();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
--- src/main/java/com/internetpackageapp/Controller.java
+++ src/main/java/com/internetpackageapp/Controller.java
@@ -1,98 +1,147 @@
 package com.internetpackageapp;
 
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXRadioButton;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
+import java.util.Objects;
 
-public class Controller implements Initializable {
 
-    private Stage stage;
-    private Scene scene;
-    private Parent root;
+public class Controller {
 
-    // Login
     @FXML
-    private JFXButton signInButton;
-    @FXML
-    private PasswordField passwordTextField;
-    @FXML
-    private TextField phoneNumberTextField;
-    @FXML
-    private Label registerLabel;
+    private JFXButton signInButton, registerButton, addQuotaButton, buy10GBHomeButton, buy20GBHomeButton, refreshQuotaButton,
+            backOnAddQuotaButton, buy10GBButton, buy1GBButton, buy20GBButton, buy5GBButton, backPaymentButton, confirmPayment1GBButton,
+            confirmPayment5GBButton, confirmPayment10GBButton, confirmPayment20GBButton, backToHomeButton, registerOnLoginButton,
+            backOnRegisterButton;
 
-    // Register
     @FXML
-    private JFXButton registerButton;
-    @FXML
-    private PasswordField passwordRegisterTextField;
-    @FXML
-    private TextField fullNameTextField;
-    @FXML
-    private TextField addressTextField;
-    @FXML
-    private TextField phoneNumberRegisterTextField;
+    private PasswordField passwordTextField, passwordRegisterTextField;
 
-    // Home
     @FXML
-    private JFXButton addQuotaButton;
-    @FXML
-    private JFXButton buy10GBHomeButton;
-    @FXML
-    private JFXButton buy20GBHomeButton;
-    @FXML
-    private JFXButton refreshQuotaButton;
+    private TextField phoneNumberTextField, fullNameTextField, addressTextField, phoneNumberRegisterTextField;
 
-    // AddQuota
     @FXML
-    private JFXButton backButton;
-    @FXML
-    private JFXButton buy10GBButton;
-    @FXML
-    private JFXButton buy1GBButton;
-    @FXML
-    private JFXButton buy20GBButton;
-    @FXML
-    private JFXButton buy5GBButton;
+    private Label registerLabel, jumlahKuotaLabel, masaAktifLabel, smsLabel, teleponLabel;
 
-    // Payment
     @FXML
-    private JFXButton backPaymentButton;
-    @FXML
-    private JFXButton confirmPaymentButton;
-    @FXML
-    private Label jumlahKuotaLabel;
-    @FXML
-    private Label masaAktifLabel;
-    @FXML
-    private ChoiceBox<?> metodePembayaranChoiceBox;
-    @FXML
-    private Label smsLabel;
-    @FXML
-    private Label teleponLabel;
+    private JFXRadioButton linkajaRadioButton, gopayRadioButton, danaRadioButton, ovoRadioButton;
 
-    // ThankYou
-    @FXML
-    private JFXButton backToHomeButton;
-
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-
+    public void LoginToHome() throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Home.fxml")));
+        Stage window = (Stage) signInButton.getScene().getWindow();
+        window.setScene(new Scene(root, 700, 500));
     }
 
-    private void switchToRegister(ActionEvent event) throws IOException {
-
+    public void LoginToRegister() throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Register.fxml")));
+        Stage window = (Stage) registerOnLoginButton.getScene().getWindow();
+        window.setScene(new Scene(root, 700, 500));
     }
+
+    public void RegisterToHome() throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Home.fxml")));
+        Stage window = (Stage) registerButton.getScene().getWindow();
+        window.setScene(new Scene(root, 700, 500));
+    }
+
+    public void RegisterToLogin() throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Login.fxml")));
+        Stage window = (Stage) backOnRegisterButton.getScene().getWindow();
+        window.setScene(new Scene(root, 700, 500));
+    }
+
+    public void HomeToAddQuota() throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("AddQuota.fxml")));
+        Stage window = (Stage) addQuotaButton.getScene().getWindow();
+        window.setScene(new Scene(root, 700, 500));
+    }
+
+    public void HomePromo10GB() throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Payment10GB.fxml")));
+        Stage window = (Stage) buy10GBHomeButton.getScene().getWindow();
+        window.setScene(new Scene(root, 700, 500));
+    }
+
+    public void HomePromo20GB() throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Payment20GB.fxml")));
+        Stage window = (Stage) buy20GBHomeButton.getScene().getWindow();
+        window.setScene(new Scene(root, 700, 500));
+    }
+
+    public void AddQuotaToHome() throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Home.fxml")));
+        Stage window = (Stage) backOnAddQuotaButton.getScene().getWindow();
+        window.setScene(new Scene(root, 700, 500));
+    }
+
+    public void AddQuota1GB() throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Payment1GB.fxml")));
+        Stage window = (Stage) buy1GBButton.getScene().getWindow();
+        window.setScene(new Scene(root, 700, 500));
+    }
+
+    public void AddQuota5GB() throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Payment5GB.fxml")));
+        Stage window = (Stage) buy5GBButton.getScene().getWindow();
+        window.setScene(new Scene(root, 700, 500));
+    }
+
+    public void AddQuota10GB() throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Payment10GB.fxml")));
+        Stage window = (Stage) buy10GBButton.getScene().getWindow();
+        window.setScene(new Scene(root, 700, 500));
+    }
+
+    public void AddQuota20GB() throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Payment20GB.fxml")));
+        Stage window = (Stage) buy20GBButton.getScene().getWindow();
+        window.setScene(new Scene(root, 700, 500));
+    }
+
+    public void PaymentToAddQuota() throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("AddQuota.fxml")));
+        Stage window = (Stage) backPaymentButton.getScene().getWindow();
+        window.setScene(new Scene(root, 700, 500));
+    }
+
+    public void ConfirmPayment1GB() throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("ThankYou.fxml")));
+        Stage window = (Stage) confirmPayment1GBButton.getScene().getWindow();
+        window.setScene(new Scene(root, 700, 500));
+    }
+
+    public void ConfirmPayment5GB() throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("ThankYou.fxml")));
+        Stage window = (Stage) confirmPayment5GBButton.getScene().getWindow();
+        window.setScene(new Scene(root, 700, 500));
+    }
+
+    public void ConfirmPayment10GB() throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("ThankYou.fxml")));
+        Stage window = (Stage) confirmPayment10GBButton.getScene().getWindow();
+        window.setScene(new Scene(root, 700, 500));
+    }
+
+    public void ConfirmPayment20GB() throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("ThankYou.fxml")));
+        Stage window = (Stage) confirmPayment20GBButton.getScene().getWindow();
+        window.setScene(new Scene(root, 700, 500));
+    }
+
+    public void ThankYouToHome() throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Home.fxml")));
+        Stage window = (Stage) backToHomeButton.getScene().getWindow();
+        window.setScene(new Scene(root, 700, 500));
+    }
+
 }
